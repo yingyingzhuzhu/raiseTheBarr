93e4c94431e91c95a922b68d81f41c84836c3737
yingyingzhuzhu <zhuyingcau@126.com>
2017-12-22 14:53:07 -0500

    import NgForm in beta.component.ts and request.component.ts

--- src/app/home/beta/beta.component.ts
+++ src/app/home/beta/beta.component.ts
@@ -1,4 +1,5 @@
-import { Component ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-beta',
--- src/app/request/request.component.ts
+++ src/app/request/request.component.ts
@@ -1,5 +1,5 @@
-import { Component ViewChild} from '@angular/core';
-//import { NgForm } from '@angular/forms';
+import { Component, ViewChild} from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-request',
